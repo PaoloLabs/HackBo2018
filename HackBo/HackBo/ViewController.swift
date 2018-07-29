77d43e835df9200c5c6036484ae5c949949b00cc
Paolo <paolo.ramos@mojix.com>
2018-07-29 00:19:41 -0400

    Adding firebase notifications

--- HackBo/HackBo/ViewController.swift
+++ HackBo/HackBo/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
