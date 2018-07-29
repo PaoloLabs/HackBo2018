3d606d807367ad55856a2ccf7a8cda12e62b4427
Hypersaiph <mygvs.mh@gmail.com>
2018-07-29 11:48:28 -0400

    pie

--- HackBo/HackBo/EstadisticasViewController.swift
+++ HackBo/HackBo/EstadisticasViewController.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import Charts
 
 class EstadisticasViewController: UIViewController {
+    @IBOutlet weak var pieChartView: PieChartView!
+    var scrollView: UIScrollView
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
+        setChart(dataPoints: months, values: unitsSold)
         // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +37,31 @@ class EstadisticasViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    func setChart(dataPoints: [String], values: [Double]) {
+        
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry1 = PieChartDataEntry(value: Double(i), label: dataPoints[i], data:  dataPoints[i] as AnyObject)
+            dataEntries.append(dataEntry1)
+            //tips en un scroll
+        }
+        
+        let pieChartDataSet = PieChartDataSet(values: dataEntries, label: "Units Sold")
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
+        pieChartView.data = pieChartData
+        
+        var colors: [UIColor] = []
+        
+        for _ in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        pieChartDataSet.colors = colors
+    }
 }
